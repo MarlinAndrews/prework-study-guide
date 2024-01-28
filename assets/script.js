1a2963f86c7add211dd51cf9836f16601cf3aeb6
Marlin Andrews <marlindandrews@gmail.com>
2024-01-28 19:09:25 +1100

    add script.js

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topic = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+// if (topic === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
+
+
+for(var x = 0; x < topic.length; x++) {
+ console.log(topic[x]);
+}
